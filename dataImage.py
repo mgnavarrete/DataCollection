e54987c9b1e85953093fcd07d8994e9af1c4a604
mgnavarrete <mgnavarrete@uc.cl>
2024-03-11 15:33:18 -0300

    a

--- dataImage.py
+++ dataImage.py
@@ -25,17 +25,18 @@ image_counts = 0
 
 print("Seleccione carpeta de imagenes...")
 
-path_root = filedialog.askdirectory(title='Seleccione el directorio raíz')
+list_folders = select_directories()
+for path_root in list_folders:
 
-# Recorrer solo las carpetas que terminan en PP
-for folder_path in os.listdir(path_root):
-    if folder_path.endswith('PP'):
-        # Recorrer todos los archivos en la carpeta
-        path = os.path.join(path_root, folder_path)
-        
-        for filename in tqdm(os.listdir(os.path.join(path,"Temp")),desc="Contando Imágenes"):
-            # Contar la cantidad de imágenes
-            image_counts += 1
+    # Recorrer solo las carpetas que terminan en PP
+    for folder_path in os.listdir(path_root):
+        if folder_path.endswith('PP'):
+            # Recorrer todos los archivos en la carpeta
+            path = os.path.join(path_root, folder_path)
+            
+            for filename in tqdm(os.listdir(os.path.join(path,"Temp")),desc="Contando Imágenes"):
+                # Contar la cantidad de imágenes
+                image_counts += 1
 
 # Imprimir los resultados
 print(f"Total de imagenes: {image_counts}")
@@ -50,7 +51,7 @@ if f"{planta}-{date}" in dataFile['Planta'].values:
     # Actualizar la fila existente
     dataFile.loc[dataFile['Planta'] == f"{planta}-{date}", 'Total Imágenes'] += image_counts
 
-    
+        
 # Guardar el archivo
 dataFile.to_csv(output_file, index=False, encoding='ISO-8859-1')
 print(f"Archivo guardado en {output_file}")
